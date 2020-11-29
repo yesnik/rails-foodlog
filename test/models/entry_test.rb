d1fe4672cf414d7b6a40ed7783874adc63ca4208
Nikita Nyatin <yesnik@yandex.ru>
2020-11-29 14:10:51 +0500

    Add entry tests

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -4,4 +4,8 @@ class EntryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'is valid with valid attributes' do
+    entry = Entry.new(meal_type: 'Breakfast', carbohydrates: 35, proteins: 20, fats: 10, calories: 400)
+    assert entry.save
+  end
 end
