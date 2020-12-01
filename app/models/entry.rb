7ac1ef2eeda036628ef2c55967372cbd94f4cceb
Nikita Nyatin <yesnik@yandex.ru>
2020-12-01 12:58:16 +0500

    Fix day method

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,6 +2,6 @@ class Entry < ApplicationRecord
   validates :calories, :proteins, :carbohydrates, :fats, :meal_type, presence: true
 
   def day
-    self.created_at.strftime("%b %e, %Y")
+    self.created_at.strftime("%b %-e, %Y")
   end
 end
