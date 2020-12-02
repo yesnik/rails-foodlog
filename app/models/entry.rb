d5aeed6957d487be6b5cfcc0efd291c453fc3259
Nikita Nyatin <yesnik@yandex.ru>
2020-12-02 15:31:20 +0500

    Add category to interface

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,5 @@
 class Entry < ApplicationRecord
-  validates :calories, :proteins, :carbohydrates, :fats, :meal_type, presence: true
+  validates :calories, :proteins, :carbohydrates, :fats, :meal_type, :category_id, presence: true
 
   belongs_to :category
 
